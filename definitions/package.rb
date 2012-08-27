710a9a40bdd05698372a7b459d75c370104fea18
Benoit Caron <bcaron@lapresse.ca>
2012-08-27 15:20:29 -0400

    idempotency for cygwin_packages

--- definitions/package.rb
+++ definitions/package.rb
@@ -11,6 +11,9 @@ define :cygwin_package, :action => "install" do
   execute "setup-cygwin-packages.exe" do
     # FIXME: how do we do this idempotently?
     #not_if {File.exists?("/etc/passwd")}
+    not_if {
+      `cygcheck -c #{params[:name]}`.include? "OK"
+    }
     cwd node['cygwin']['download_path'] 
     command "setup.exe -q -O -R #{node['cygwin']['home']} -s #{node['cygwin']['site']} #{proxycmd} -P #{params[:name]}"
     action :run
