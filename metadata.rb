39f6e93be6999b126242c2d2c68000b8fdbce51e
Benoit Caron <bcaron@lapresse.ca>
2013-08-29 12:54:26 -0400

    Fake bump, messed up tags already pushed

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "bcaron@lapresse.ca"
 license          "Apache 2.0"
 description      "Installs/Configures cygwin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.5"
+version          "0.2.6"
 
